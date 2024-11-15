e0886cb072bb5ec4dae288db9bf5b287db19d9a0
Mustafa Omar <themustafaomar@gmail.com>
2024-11-15 22:27:52 +0200

    chore: remove any keyword from marker draggable example

--- docs/examples/components/marker-draggable.vue
+++ docs/examples/components/marker-draggable.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
 import { shallowRef } from 'vue'
-import { Map, Marker } from 'libregl'
-import { LngLatLike } from 'maplibre-gl'
+import { Map, Marker, type MarkerEvent } from 'libregl'
+import type { LngLatLike } from 'maplibre-gl'
 
 const style = `https://api.maptiler.com/maps/streets-v2/style.json?key=${import.meta.env.VITE_MAPTILER_KEY}`
 
 const coordinates = shallowRef<LngLatLike>([19.834688, 50.294592])
 
-const onDragStart = (event: any) => {
+const onDragStart = (event: MarkerEvent) => {
   console.log('onDragStart()', event)
 }
 
-const onDrag = (event: any) => {
+const onDrag = (event: MarkerEvent) => {
   console.log('onDrag()', event)
 }
 
-const onDragEnd = (event: any) => {
+const onDragEnd = (event: MarkerEvent) => {
   console.log('onDragEnd()', event)
   alert(`onDragEnd([${event.lngLat.lng}, ${event.lngLat.lat}])`)
 }
