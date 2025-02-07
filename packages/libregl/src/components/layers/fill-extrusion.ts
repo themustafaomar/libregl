920aa55cbd5c0900287592818cd67ccb8ad7549e
Mustafa Omar <themustafaomar@gmail.com>
2025-02-07 13:41:11 +0200

    chore: drop the unnecessary spread operator (#9)

--- packages/libregl/src/components/layers/fill-extrusion.ts
+++ packages/libregl/src/components/layers/fill-extrusion.ts
@@ -12,9 +12,7 @@ const UID_PREFIX = `${TYPE}-layer`
 export const FillExtrusionLayer = defineComponent({
   name: 'FillExtrusionLayer',
   emits: Object.assign(layerEvents, []),
-  props: {
-    ...props<FillExtrusionLayerSpecification, GeoJSONSourceSpecification | string>(UID_PREFIX),
-  },
+  props: props<FillExtrusionLayerSpecification, GeoJSONSourceSpecification | string>(UID_PREFIX),
   setup(props, { expose }) {
     const { map, layer } = useLayer<FillExtrusionLayerSpecification>(TYPE, props)
 
