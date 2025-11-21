2fc1ff265d2af9a3f51264992d3e4881493e8f97
Mustafa Omar <themustafaomar@gmail.com>
2025-11-21 19:37:27 +0200

    chore(playground): update controls example

--- packages/playground/src/pages/controls/index.vue
+++ packages/playground/src/pages/controls/index.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { Map, GlobeControl } from 'libregl'
-
-const mapRef = ref()
+import {
+  Map,
+  ScaleControl,
+  GeolocateControl,
+  GlobeControl,
+  FullscreenControl,
+  LogoControl,
+  NavigationControl,
+  AttributionControl
+} from 'libregl'
 </script>
 
 <template>
   <Map
-    ref="mapRef"
-    projection="globe"
     height="100vh"
-    width="100%"
     :style="'https://api.maptiler.com/maps/streets-v2/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL'"
+    :zoom="4"
+    :attribution-control="false"
   >
-    <GlobeControl position="top-right" />
+    <AttributionControl />
+    <NavigationControl />
+    <FullscreenControl />
+    <GeolocateControl
+      @trackuserlocationend="console.log('trackuserlocationend', $event)"
+      @trackuserlocationstart="console.log('trackuserlocationstart', $event)"
+      @userlocationlostfocus="console.log('userlocationlostfocus', $event)"
+      @userlocationfocus="console.log('userlocationfocus', $event)"
+      @geolocate="console.log('geolocate', $event)"
+      @error="console.log('error', $event)"
+      @outofmaxbounds="console.log('outofmaxbounds', $event)"
+    >
+    </GeolocateControl>
+    <GlobeControl />
+    <LogoControl />
+    <ScaleControl position="bottom-right" />
   </Map>
 </template>
